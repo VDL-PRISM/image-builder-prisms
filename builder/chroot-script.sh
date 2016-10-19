d8ea43a85f18409ce232404d957ee45603684f93
Philip Lundrigan <philiplundrigan@gmail.com>
2016-10-19 14:05:38 -0600

    Install home assistant, mosquitto, and influxdb

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -11,7 +11,26 @@ apt-get install -y \
   python3-venv \
   python3-pip
 
-# TODO: Install mosquitto
+# Create home assistant user
+groupadd -f -r -g 1001 homeassistant
+useradd -u 1001 -g 1001 -rm homeassistant
+
+# Install Home Assistant
+python3 -m venv /srv/homeassistant && \
+  chown -R homeassistant:homeassistant /srv/homeassistant && \
+  su homeassistant -s /bin/bash -c " source /srv/homeassistant/bin/activate && pip3 install homeassistant==${HOME_ASSISTANT_VERSION}" && \
+  systemctl enable home-assistant@homeassistant.service
+
+# TODO: Install all of Home Assistant dependencies
+
+# Install mosquitto
+apt-get install -y \
+  mosquitto
+
+# Install influxdb
+wget -q https://dl.influxdata.com/influxdb/releases/influxdb_${INFLUXDB_VERSION}_armhf.deb
+sudo dpkg -i influxdb_${INFLUXDB_VERSION}_armhf.deb
+
 # TODO: Rename host name?
 # TODO: Rename user?
 
