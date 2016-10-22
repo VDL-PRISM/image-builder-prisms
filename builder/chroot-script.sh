3dca0d10370c405f4e7ff7156172aa49a4d38fcc
Philip Lundrigan <philiplundrigan@gmail.com>
2016-10-22 16:25:31 -0600

    Uncomment everything in chroot script

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -2,51 +2,51 @@
 set -ex
 
 # Set default locales to 'en_US.UTF-8'
-# echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-# locale-gen
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
 
-# echo 'locales locales/default_environment_locale select en_US.UTF-8' | debconf-set-selections
-# dpkg-reconfigure -f noninteractive locales
+echo 'locales locales/default_environment_locale select en_US.UTF-8' | debconf-set-selections
+dpkg-reconfigure -f noninteractive locales
 
 # reload package sources
 apt-get update
-# apt-get upgrade -y
+apt-get upgrade -y
 
-# # Install Python 3 for Home Assistant
-# apt-get install -y \
-#   python3 \
-#   python3-venv \
-#   python3-pip
+# Install Python 3 for Home Assistant
+apt-get install -y \
+  python3 \
+  python3-venv \
+  python3-pip
 
-# # Create home assistant user
-# groupadd -f -r -g 1001 homeassistant
-# useradd -u 1001 -g 1001 -rm homeassistant
+# Create home assistant user
+groupadd -f -r -g 1001 homeassistant
+useradd -u 1001 -g 1001 -rm homeassistant
 
-# # Install Home Assistant
-# python3 -m venv /srv/homeassistant
-# chown -R homeassistant:homeassistant /srv/homeassistant
-# su homeassistant -s /bin/bash -c "source /srv/homeassistant/bin/activate && pip3 -V && pip install --upgrade pip"
-# su homeassistant -s /bin/bash -c "source /srv/homeassistant/bin/activate && pip3 install --no-cache-dir homeassistant==${HOME_ASSISTANT_VERSION}"
-# systemctl enable home-assistant@homeassistant.service
+# Install Home Assistant
+python3 -m venv /srv/homeassistant
+chown -R homeassistant:homeassistant /srv/homeassistant
+su homeassistant -s /bin/bash -c "source /srv/homeassistant/bin/activate && pip3 -V && pip install --upgrade pip"
+su homeassistant -s /bin/bash -c "source /srv/homeassistant/bin/activate && pip3 install --no-cache-dir homeassistant==${HOME_ASSISTANT_VERSION}"
+systemctl enable home-assistant@homeassistant.service
 
-# # TODO: Install all of Home Assistant dependencies
+# TODO: Install all of Home Assistant dependencies
 
-# # Clean up Python caches
-# find /srv/homeassistant/lib/ | \
-#   grep -E "(__pycache__|\.pyc$)" | \
-#   xargs rm -rf
+# Clean up Python caches
+find /srv/homeassistant/lib/ | \
+  grep -E "(__pycache__|\.pyc$)" | \
+  xargs rm -rf
 
-# # Install mosquitto
-# apt-get install -y \
-#   mosquitto
+# Install mosquitto
+apt-get install -y \
+  mosquitto
 
-# # Install influxdb
-# wget -q "https://dl.influxdata.com/influxdb/releases/influxdb_${INFLUXDB_VERSION}_armhf.deb"
-# sudo dpkg -i "influxdb_${INFLUXDB_VERSION}_armhf.deb"
+# Install influxdb
+wget -q "https://dl.influxdata.com/influxdb/releases/influxdb_${INFLUXDB_VERSION}_armhf.deb"
+sudo dpkg -i "influxdb_${INFLUXDB_VERSION}_armhf.deb"
 
-# # TODO: Rename host name?
-# # TODO: Rename user and password?
+# TODO: Rename host name?
+# TODO: Rename user and password?
 
-# # Cleanup APT cache and lists
-# apt-get clean
-# rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# Cleanup APT cache and lists
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
