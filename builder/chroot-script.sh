4ac6eb5eb9982c9250c5ba6b3d413d7a1164e247
Philip Lundrigan <philiplundrigan@gmail.com>
2016-10-23 08:32:35 -0600

    Shorten build time for testing
    Remove most of everything from chroot script. Trying to figure out why
    /etc/sudoers doesn't exist when influxdb installs.

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -3,22 +3,24 @@ set -ex
 
 uname -a
 
-# Set default locales to 'en_US.UTF-8'
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen
+cat /etc/sudoers
 
-echo 'locales locales/default_environment_locale select en_US.UTF-8' | debconf-set-selections
-dpkg-reconfigure -f noninteractive locales
+# Set default locales to 'en_US.UTF-8'
+# echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+# locale-gen
+
+# echo 'locales locales/default_environment_locale select en_US.UTF-8' | debconf-set-selections
+# dpkg-reconfigure -f noninteractive locales
 
 # reload package sources
 apt-get update
 # apt-get upgrade -y
 
 # Install Python 3 for Home Assistant
-apt-get install -y \
-  python3 \
-  python3-venv \
-  python3-pip
+# apt-get install -y \
+#   python3 \
+#   python3-venv \
+#   python3-pip
 
 # Create home assistant user
 groupadd -f -r -g 1001 homeassistant
@@ -29,24 +31,26 @@ for GRP in video gpio i2c spi; do
   adduser homeassistant $GRP
 done
 
-# Install Home Assistant
-python3 -m venv /srv/homeassistant
-source /srv/homeassistant/bin/activate
-pip3 install --upgrade pip
-pip3 install --no-cache-dir homeassistant==${HOME_ASSISTANT_VERSION}
-# TODO: Install all of Home Assistant dependencies
-# pip3 install --no-cache-dir -r https://raw.githubusercontent.com/home-assistant/home-assistant/dev/requirements_all.txt
-chown -R homeassistant:homeassistant /srv/homeassistant
-systemctl enable home-assistant@homeassistant.service
+# # Install Home Assistant
+# python3 -m venv /srv/homeassistant
+# source /srv/homeassistant/bin/activate
+# pip3 install --upgrade pip
+# pip3 install --no-cache-dir homeassistant==${HOME_ASSISTANT_VERSION}
+# # TODO: Install all of Home Assistant dependencies
+# # pip3 install --no-cache-dir -r https://raw.githubusercontent.com/home-assistant/home-assistant/dev/requirements_all.txt
+# chown -R homeassistant:homeassistant /srv/homeassistant
+# systemctl enable home-assistant@homeassistant.service
 
-# Clean up Python caches
-find /srv/homeassistant/lib/ | \
-  grep -E "(__pycache__|\.pyc$)" | \
-  xargs rm -rf
+# # Clean up Python caches
+# find /srv/homeassistant/lib/ | \
+#   grep -E "(__pycache__|\.pyc$)" | \
+#   xargs rm -rf
 
 # Install mosquitto
 apt-get install -y \
   mosquitto
+
+cat /etc/sudoers
 
 # Install influxdb
 wget -q "https://dl.influxdata.com/influxdb/releases/influxdb_${INFLUXDB_VERSION}_armhf.deb"
